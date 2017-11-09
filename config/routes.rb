03d7a1ef4ccc237779c9ef563f09353706ab8d82
Adriene Paula <adriene_paula@estudante.sc.senai.br>
2017-11-09 21:43:56 +0000

    task

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  resources :tasks do
+    member do 
+       put :change
+   end
+  end 
   devise_for :users
   get 'pages/home'
-  
   get 'about' => 'pages#about'
+  get 'test'=>'pages#test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
